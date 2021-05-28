ed38545393cf4d84f97e824e0f1cf713aebc232c
u090303 <student090303@gmail.com>
2021-05-28 19:13:48 +0300

    create social icons template

---
+++ local/templates/main/components/bitrix/news.list/footer_socials/template.php
@@ -0,0 +1,25 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+?>
+<div class="social">
+	<ul>
+	<?foreach($arResult["ITEMS"] as $arItem):?>
+		<?
+		$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+		$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+		?>
+		<li id="<?=$this->GetEditAreaId($arItem['ID']);?>"><a href="<?=$arItem["PROPERTIES"]['URL']['VALUE']?>" target="_blank" rel="nofollow"><img src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>" width="26" height="27" alt="" class="vm"/><?echo $arItem["NAME"]?></a></li>
+	<?endforeach;?>
+	</ul>
+</div>
