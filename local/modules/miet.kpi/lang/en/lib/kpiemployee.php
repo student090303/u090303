c385a6e5371c9c929f3cbf12dfcb8bf9a0cc87f8
u090303 <student090303@gmail.com>
2021-05-29 14:06:17 +0300

    DAO KPI employee has been created

---
+++ local/modules/miet.kpi/lang/en/lib/kpiemployee.php
@@ -0,0 +1,6 @@
+<?
+$MESS["KPI_ENTITY_ID_FIELD"] = "ID KPI";
+$MESS["KPI_ENTITY_UF_VALUE_FIELD"] = "Indicator value";
+$MESS["KPI_ENTITY_UF_CREATED_FIELD"] = "Date when the indicator value was entered";
+$MESS["KPI_ENTITY_UF_CHANGED_FIELD"] = "Date when the metric value was last changed";
+$MESS["KPI_DEPARTMENT_V_ZERO"] = "The quantity must be greater than zero";
