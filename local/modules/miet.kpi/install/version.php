6c42d92f024586071b703ca569a2b4b1ef4372a3
u090303 <student090303@gmail.com>
2021-05-29 13:38:55 +0300

    KPI module skeleton has been created

---
+++ local/modules/miet.kpi/install/version.php
@@ -0,0 +1,5 @@
+<?
+$arModuleVersion = array(
+	"VERSION" => "1.0.0",
+	"VERSION_DATE" => "2021-05-29 17:30:00"
+);
---
+++ local/modules/miet.kpi/lang/en/install/index.php
@@ -0,0 +1,5 @@
+<?
+$MESS["miet.kpi_MODULE_NAME"] = "MIET. KPI";
+$MESS["miet.kpi_MODULE_DESC"] = "Evaluating the KPIs of employees and departments";
+$MESS["miet.kpi_PARTNER_NAME"] = "MIET";
+$MESS["miet.kpi_PARTNER_URI"] = "http://miet.ru/";
